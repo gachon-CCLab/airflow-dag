72ab3b2ae6157c907f3ed50df75b3180724dd35c
Semo <tpah20@gmail.com>
2023-01-25 11:01:54 +0900

    update label

--- dags/flower-ios-server.py
+++ dags/flower-ios-server.py
@@ -94,7 +94,7 @@ run = KubernetesPodOperator(
         python3 -m pip install -r /app/requirements.txt; \
         python3 /app/app.py;"],
     ports=[port],
-    labels={'run':'fl-android-server'},
+    labels={'run':'fl-ios-server'},
     env_vars={'REPO_URL':'https://github.com/gachon-CCLab/flower_ios_server.git',
               "GIT_TAG":"master" ,
               "ENV": 'init' },
