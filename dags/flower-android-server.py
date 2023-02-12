f3ba9518561eae253a99613532eef3e9fd19f87d
Semo <tpah20@gmail.com>
2023-02-12 22:51:56 +0900

    flscalize test

--- dags/flower-android-server.py
+++ dags/flower-android-server.py
@@ -9,7 +9,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 
-dag_id = 'fl-android-server-dag'
+dag_id = 'fl-fashion-mnist-dag'
 
 now = datetime.now()
 
